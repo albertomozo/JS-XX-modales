a443e564bce0c2eb289b12ffe0978c10b40b290f
Alberto Mozo <albertomozodesarrollador@gmail.com>
2024-04-26 20:17:15 +0200

    primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ventanas modales
+
+## <dialog>
+### 04-dialog_00.html
+dialog básico
+
+
+### 04-dialog_05.html
+botonoes personalizados
---
+++ modal/datos.js
@@ -0,0 +1,22 @@
+const flores = [
+    {
+        "nombre" : "Hortensia",
+        "descripcion" : "Flor arbustiva que necesita humedad",
+        "imagen" : "img/imagen1.jpg",
+        "galeria" : ["img/imagen8.jpg","img/iamgen9.jpg"],
+        "precio" : 3,
+    },
+    {
+        "nombre" : "Lirio",
+        "descripcion" : "Bulbo que florece a primeros de primavera",
+        "imagen" : "img/imagen2.jpg",
+        "precio" : 12,
+    },
+    {
+        "nombre" : "Tulipan",
+        "descripcion" : "Tulipan negro  de primavera",
+        "imagen" : "img/imagen2.jpg",
+        "precio" : 6,
+    },
+
+]
